738d31ba236e9213708757fc0f7a53c3e56c3176
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-16 13:22:58 -0600

    got arbitrary types between ts and rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,26 @@
+#[derive(serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Game {
+    pub name: String,
+    pub launch_command: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {name}! You've been greeted from Rust!")
+}
+
+#[tauri::command]
+fn add_game(game: Game) {
+    println!("{game:?}");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, add_game])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
