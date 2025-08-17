02f0ae193731518bf591b1e2a50cff426570770d
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-17 12:15:04 -0600

    better error popup

--- src/routes/ErrorPopup.svelte
+++ src/routes/ErrorPopup.svelte
@@ -13,8 +13,17 @@
   <div
     class="fixed top-0 left-0 w-full h-full p-12 backdrop-blur-md text-white"
   >
-    <div class="p-4 h-full bg-slate-500 flex flex-col gap-4">
-      {message}
+    <div
+      class="p-4 h-full bg-slate-500 flex flex-col gap-4 text-center justify-between"
+    >
+      <h1 class="text-2xl">An Error Occured</h1>
+      <p>
+        {message}
+      </p>
+      <button
+        class="p-4 w-full bg-red-400 cursor-pointer"
+        onclick={() => (menuOpen = false)}>Close</button
+      >
     </div>
   </div>
 {/if}
