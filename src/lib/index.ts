8f28b08c9a38b37075ada3334baf2a74bd02e2ee
Shuflduf <shuflduf@shuflduf.xyz>
2025-08-17 14:17:08 -0600

    more comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,7 @@
+// Seperate file so that every Svelte component has access to it easily
+// This has to have the same structure as the Game struct in Rust,
+// and unless you want to do rename_all stuff, it's the simplest way.
+// https://v2.tauri.app/develop/calling-rust/#passing-arguments
 export type Game = {
   name: string;
   launch_command: string;
